e93aafccc7e17ee9909dfe820a7e2be2fb6025fe
colour-of-art <149279801+colour-of-art@users.noreply.github.com>
2023-12-13 23:42:53 +1100

    Update advent01part1.js

--- 01/advent01part1.js
+++ 01/advent01part1.js
@@ -4,28 +4,16 @@ const readline = require('readline');
 const fileStream = fs.createReadStream('input.txt');
 
 const rl = readline.createInterface({
-  input: fileStream,
-  crlfDelay: Infinity
+    input: fileStream,
+    crlfDelay: Infinity
 });
 
 let sum = 0;
 rl.on('line', (line) => {
-    let num = ""; 
-    for (let i = 0; i < line.length; i++){
-        if (line[i] >= '0' && line[i] <= '9'){
-            num += line[i];
-            break;
-        }
-    }
-    for (let i = line.length-1; i >= 0; i--){
-        if (line[i] >= '0' && line[i] <= '9'){
-            num += line[i];
-            break;
-        }
-    }
-    sum += parseInt(num);
+    let numbers = line.split('').filter(char => char.toLowerCase() === char.toUpperCase());
+    sum += parseInt(numbers[0] + numbers[numbers.length-1]);
 });
 
 rl.on('close', () => {
-  console.log(sum);
+    console.log(sum);
 });
